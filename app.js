c26ef55312b33e839a47c408e2bda56dfaa875d0
anmolcheema836 <anmolcheema988@gmail.com>
2026-03-25 08:53:09 +0530

    gf

---
+++ app.js
@@ -0,0 +1,11 @@
+async function readFiles() {
+  try {
+    console.log('1. Starting to read files...');
+    const data1 = await fs.readFile('file1.txt', 'utf8');
+    const data2 = await fs.readFile('file2.txt', 'utf8');
+    console.log('2. Files read successfully!');
+    return { data1, data2 };
+  } catch (error) {
+    console.error('Error reading files:', error);
+  }
+}
